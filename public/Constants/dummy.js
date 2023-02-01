cd9a9c2df80d71ed37e58084317e9f581e8645c6
Mansour-VN <pirouz.vn@gmail.com>
2023-02-01 19:24:49 +0330

    feat : add dummy in project

---
+++ public/Constants/dummy.js
@@ -0,0 +1,89 @@
+//Social networks icons
+import Whatsapp from "./DummyAssets/whatsapp-icon.svg";
+import Telegram from "./DummyAssets/telegram.svg";
+import Twitter from "./DummyAssets/twitter-circle-filled.svg";
+import Instagram from "./DummyAssets/instagram-with-circle.svg";
+import Linkedin from "./DummyAssets/linkedin-with-circle.svg";
+import Aparat from "./DummyAssets/Aparat.svg";
+import Youtube from "./DummyAssets/youtube-with-circle.svg";
+import Bale from "./DummyAssets/bale.svg";
+import Soroush from "./DummyAssets/Soroush logo";
+import Eitaa from "./DummyAssets/Eitaa.svg";
+
+//Subsidiary companies
+import Gomrokapp from "./DummyAssets/Gomrokapp.jpg";
+import DianHamrah_circle from "./DummyAssets/DianHamrah.jpg";
+import DIAN_rectangel from "./DummyAssets/DIAN.png";
+import Bimehapp from "./DummyAssets/Bimehapp.jpg";
+import Adlapp from "./DummyAssets/Adlapp.jpg";
+
+//Icons
+import search_icon from "";
+import point_icon from "./DummyAssets/point";
+import book_icon from "./DummyAssets/book";
+import x_icon from "./DummyAssets/x.svg";
+import telephone_icon from "./DummyAssets/telephone-in-talk.svg";
+import privacy_icon from "./DummyAssets/privacy-tip.svg";
+import menu_icon from "./DummyAssets/menu.svg";
+import home_icon from "./DummyAssets/home-rounded.svg";
+import search_icon from "./DummyAssets/baseline-search.svg";
+import email_icon from "./DummyAssets/alternate-email-rounded.svg";
+
+
+const socialNetwork = {
+  "شبکه‌های اجتماعی": {
+    Whatsapp: [Whatsapp, "https://chat.whatsapp.com/G0o3ZKS1EHpEhbGtevLhRs"],
+    Telegram: [Telegram, "https://t.me/dianhamrahefarda"],
+    Twitter: [Twitter, link],
+    Instagram: [Instagram,link ],
+    Linkedin: [Linkedin, link],
+    Apart: [Aparat, link],
+    Youtube: [Youtube, link],
+    Bale: [Bale, link],
+    Eitaa: [Eitaa, link],
+    Soroush: [Soroush, link],
+  },
+};
+export const constant = {
+  آدرس: "تهران، میدان صنعت، خیابان دادمان، کوچه میلاد چهارم،پلاک ۱۵",
+  کدپستی: "1468644451",
+  "صندوق پستی": "",
+  تلفن: "021-96646",
+  ایمیل: "info@dianhamrah.ir",
+  "ساعات کاری": ["شنبه تا چهارشنبه از ساعت ۸ الی ۱۷","پنج شنبه ها از ساعت ۸ الی ۱۴"],
+  socialNetwork,
+};
+export const navbar = {
+  "دیان همراه": icon,
+  menu:["آساک","محصولات", "خدمات","آساکی شو", "تماس با ما" ],
+};
+export const header = {};
+export const footer = {
+  "خدمات مشتریان": { "حریم خصوصی": "", "پاسخ‌به پرسش‌های متداول": {} },
+  پیوندها: { "صفحه اصلی": "", "درباره ما": "", "تماس با ما": "" },
+  "سایت‌های مرتبط": {
+    مالیاپ: "https://www.maliapp.ir/",
+    "بیمه اپ": "https://bimehapp.com/",
+    "گمرک اپ": "http://www.gomrokapp.ir/",
+    "عدل اپ": "http://www.adlapp.ir/",
+    "دیان همراه": "https://dianhamrah.ir/",
+  },
+  "پیوندهای سازمانی": {
+    "وزارت امور اقتصادی و دارایی":
+      "https://www.mefa.ir/fa-IR/mefa/1/page/%D8%B5%D9%81%D8%AD%D9%87-%D8%A7%D8%B5%D9%84%DB%8C",
+    "سازمان امور مالیاتی کشور": "https://www.intamedia.ir/",
+    "اخبار وزیر امور اقتصادی و دارایی": "http://www.intamedia.ir/ministry-news",
+    "راهبرد مشارکت سازمان امور مالیاتی کشور":
+      "https://www.intamedia.ir/organizational-participation-strategy",
+    "درگاه ملی خدمات الکترونیکی مالیاتی": "https://my.tax.gov.ir/",
+    "سامانه مدیریت اطلاعات پژوهشی سازمان امور مالیاتی کشور}":
+      "https://taxresearch.ir/",
+  },
+  مجوزها: {
+    "مجوز نوع یک":
+      "https://www.intamedia.ir/Portals/0/news/2021/%D8%AC%D8%AF%D9%88%D9%84%20%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA%20%D8%B4%D8%B1%DA%A9%D8%AA%20%D9%87%D8%A7%DB%8C%20%D9%85%D8%B9%D8%AA%D9%85%D8%AF_6.pdf?ver=kzrLl-cb-0kFHUsYAg67WQ%3d%3d",
+  },
+};
+export const slogan = {};
+
+export const articles = { category: { title: "" } };

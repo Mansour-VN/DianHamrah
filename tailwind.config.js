5f9225aa73f037929a16c4c7f9da03dcef3fd5cf
Mansour-VN <pirouz.vn@gmail.com>
2023-02-02 10:01:32 -0800

    refactor : refactor taillwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+        screens: {
+            'sm': '576px',
+            // => @media (min-width: 576px) { ... }
+
+            'md': '960px',
+            // => @media (min-width: 960px) { ... }
+
+            'lg': '1140px',
+            // => @media (min-width: 1140px) { ... }
+        },
+        colors: {
+            "official": "#f6821f",
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+    },
+    plugins: [],
 }

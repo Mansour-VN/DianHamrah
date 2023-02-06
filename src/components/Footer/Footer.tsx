fec2649aaa04c0b901f188b0756726f87fb9c782
Mansour-VN <pirouz.vn@gmail.com>
2023-02-06 10:54:56 +0330

    feat : add daisyUI and section3

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,44 +2,44 @@ import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <div className='w-full bg-gray-700 flex items-center justify-center relative flex-col' >
-      <div className='z-0'>
-        <Image
-          src={"/assets/images/background/f-bg.png"}
-          alt=""
-          width={1900}
-          height={400}
-        />
-      </div>
-      <div id='footerr' className='flex flex-col md:flex-row  container justify-around md:absolute md:h-80 z-1'>
-
-        <div id='About' className='flex flex-col '>
-            <h3 className='border-b-4 font-bold text-white p-4'>دیان‌همراه‌فردا</h3>
-            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
-        </div>
-
-        <div id='Link' className='flex flex-col'>
-            <h3 className='border-b-4 font-bold text-white p-4'>سایت‌های مربوطه</h3>
-            <p>لورم ایپسوم متن ساختگی با تولید    صنعت  وگرافیک است</p>
-            <p>لورم ایپسوم متن ساختگی با تولید    صنعت  وگرافیک است</p>
-            <p>لورم ایپسوم متن ساختگی با تولید    صنعت  وگرافیک است</p>
-        </div>
-
-        <div id='location social' className='flex flex-col'>
-            <h3 className='border-b-4 font-bold text-white p-4'>دسترسی ما</h3>
-            <div className=''>
-                Location
+    return (
+        <div className='w-full bg-gray-700 flex items-center justify-center relative flex-col'>
+            <div className='z-0'>
+                <Image
+                    src={"/assets/images/background/f-bg.png"}
+                    alt=""
+                    width={1900}
+                    height={400}
+                />
             </div>
-            <div>
-              social Media
+            <div id='footerr' className='flex flex-col md:flex-row  container justify-around md:absolute md:h-80 z-1'>
+
+                <div id='About' className='flex flex-col '>
+                    <h3 className='border-b-4 font-bold text-white p-4'>دیان‌همراه‌فردا</h3>
+                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+                </div>
+
+                <div id='Link' className='flex flex-col'>
+                    <h3 className='border-b-4 font-bold text-white p-4'>سایت‌های مربوطه</h3>
+                    <p>لورم ایپسوم متن ساختگی با تولید صنعت وگرافیک است</p>
+                    <p>لورم ایپسوم متن ساختگی با تولید صنعت وگرافیک است</p>
+                    <p>لورم ایپسوم متن ساختگی با تولید صنعت وگرافیک است</p>
+                </div>
+
+                <div id='location social' className='flex flex-col'>
+                    <h3 className='border-b-4 font-bold text-white p-4'>دسترسی ما</h3>
+                    <div className=''>
+                        Location
+                    </div>
+                    <div>
+                        social Media
+                    </div>
+                </div>
+
             </div>
+
         </div>
-
-      </div>
-
-    </div>
-  )
+    )
 }
 
 export default Footer
--- src/pages/HomePage/Section3.tsx
+++ src/pages/HomePage/Section3.tsx
@@ -1,7 +1,73 @@
+import Image from "next/image";
+import about_img from "../../../public/assets/images/background/about_img.png"
+
 const Section3 = () => {
-  return (
-    <div>Section3 Mansour</div>
-  )
+
+    const counter =(num=0)=>{
+
+            console.log(num)
+            return(
+                setTimeout(()=> {
+                    if(num<285){
+                        num++
+                    }
+                },0.5)
+            )
+
+    }
+
+    return (
+        <div>Section3 Mansour
+            <Image src={about_img} alt="about"/>
+            <div>
+                <div className="">
+                    <div className="about-content s-about-content">
+                        <div className="about-title second-atitle pb-40">
+                            <h5> راهی جدید برای رشد پیدا کنید</h5>
+                            <h2>بهترین راه حل کسب و کار خود را با قالب ما دریافت کنید</h2>
+                            <p>طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند که محتوای اصلی صفحات
+                                آماده
+                                نیست. در نتیجه طرح کلی دید درستی به کار فرما نمیدهد. اگر طراح بخواهد دنبال متن های مرتبط
+                                بگردد تمرکزش از روی کار اصلی برداشته میشود و اینکار زمان بر خواهد بود. همچنین طراح به
+                                دنبال
+                                این است که پس از ارایه کار نظر دیگران را در مورد طراحی جویا شود.
+
+                            </p>
+                        </div>
+                        <ul className="progress-outer">
+                            <li>
+                                <div className="progress-box">
+
+
+                                    <div className="stats shadow">
+
+                                        <div className="stat">
+                                            <div className="stat-title">تعداد مشتریان</div>
+                                            <div className="stat-value"></div>
+                                            <div className="stat-desc">22% بیشتر از ماه قبل</div>
+                                        </div>
+
+                                    </div>
+
+
+                                    <h4>راه حل تجاری</h4>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className="progress-box mt-50">
+
+                                    <h4>رشد کسب و کار</h4>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+)
 }
 
 export default Section3

f5083c4a9afd9519cb2b081bc56e05ca96ab7501
morteza <morteza.abolfathi73@gmail.com>
2023-02-06 17:59:26 +0330

    refactor : z-index menu & add Logo dian

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: {
 }) {
     return (
         <>
-            <div className="header_Layout sticky top-0 z-10">
+            <div className="header_Layout sticky top-0 z-50">
                 <Header />
             </div>
             <main>{children}</main>

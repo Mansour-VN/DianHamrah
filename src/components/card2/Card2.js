9a28a3b8fb2c5bb3e4c66cf48f19ffa18579fc21
morteza <morteza.abolfathi73@gmail.com>
2023-02-08 13:48:37 +0330

    refactor: change design card2

--- src/components/card2/Card2.js
+++ src/components/card2/Card2.js
@@ -3,21 +3,15 @@ import Image from 'next/image';
 
 const Card2 = ({src, textTitleCard, mainTextCard}) => {
     return (
-        <div id="card2" className="flex flex-col gap-2 w-80 bg-gray-100">
-            <div>
-                <Image
-                    src={src}
-                    alt=""
-                    width={500}
-                    height={500}
-                />
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={src} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title text-official text-center">{textTitleCard}</h2>
+                <p className="text-justify">{mainTextCard}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">اطلاعات بیشتر</button>
+                </div>
             </div>
-
-            <div className='px-3'>
-                <h3 className='text-xl text-official font-bold p-1 text-center'>{textTitleCard}</h3>
-                <p className='font-semibold text-justify p-2'>{mainTextCard}</p>
-            </div>
-
         </div>
     )
 }

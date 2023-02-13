228f777d4e6106ca6f805a71126fb5846f1b5d23
Mansour-VN <pirouz.vn@gmail.com>
2023-02-13 14:44:12 +0330

    feat : add AboutUs page and components

---
+++ src/pages/AboutUs/index.tsx
@@ -0,0 +1,20 @@
+import Section1 from "@/pages/AboutUs/Section1";
+import Section2 from "@/pages/HomePage/Section6";
+import Section4 from "@/pages/HomePage/Section7";
+import Section5 from "@/pages/HomePage/Section8";
+import Section3 from "@/pages/AboutUs/Section3";
+
+
+const AboutUs = () => {
+
+    return (
+        <div id="sec1" className="bg-sky-50 ">
+            <Section1/>
+            <Section2/>
+            <Section3/>
+            <Section4/>
+            <Section5/>
+        </div>
+    )
+}
+export default AboutUs

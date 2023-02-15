9bd194ddec53de534ec3179a16ea288866a44953
morteza <morteza.abolfathi73@gmail.com>
2023-02-15 18:57:47 +0330

    feat: add animate Text

--- src/pages/HomePage/Section1.tsx
+++ src/pages/HomePage/Section1.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { TypeAnimation } from 'react-type-animation';
+
+
 const section1 = () => {
 
     return (
@@ -10,11 +13,29 @@ const section1 = () => {
                 autoPlay
                 className="opacity-70"
             />
+
+            <div className="absolute bg-gray-100 opacity-70 rounded-md md:p-4">
+                <TypeAnimation
+                    // Same String at the start will only be typed once, initially
+                    sequence={[
+                        'شرکت دیان‌ همراه‌ فردا',
+                        1000,
+                        'شرکت دیان‌ همراه‌ فردا اولین و تنها‌ترین معتمد نوع دو و سه سازمان‌ امور مالیاتی کشور',
+                        1000,
+                        'شرکت دیان‌ همرا‌ه فردا شما را آموزش می‌دهد که خودتان صورت حساب ارسال کنید',
+                        1000,
+                        'شرکت دیان همراه فردا با پشتیبانی تیم فنی در کنار شما است',
+                        1000,
+                    ]}
+                    speed={50} // Custom Speed from 1-99 - Default Speed: 40
+                    // style={{ fontSize: '2em' }}
+                    wrapper="span" // Animation will be rendered as a <span>
+                    repeat={Infinity} // Repeat this Animation Sequence infinitely
+                    className="text-xl md:text-3xl font-semibold"
+                />
+            </div>
         </div>
     )
 }
 
 export default section1
-
-
-{/* <div id="30885699973"><script type="text/JavaScript" src="https://www.aparat.com/embed/VMjwe?data[rnddiv]=30885699973&data[responsive]=yes&muted=true&autoplay=true"></script></div> */ }

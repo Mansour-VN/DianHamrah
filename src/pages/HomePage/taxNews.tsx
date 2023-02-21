571e66bc13aaa03200b6d59a5c3623e8c8c3077a
morteza <morteza.abolfathi73@gmail.com>
2023-02-21 09:53:40 +0330

    style: margin card in taxNews

--- src/pages/HomePage/taxNews.tsx
+++ src/pages/HomePage/taxNews.tsx
@@ -6,7 +6,7 @@ const Section8 = () => {
         <div id="sec8" className="w-full flex flex-col items-center relative z-0 pt-4">
             <h1 className="text-4xl font-bold py-4	text-center	m-4 sticky w-full text-black">اخبارمالیاتی </h1>
             <div className="container">
-                <div className="flex md:flex-row  md:gap-12 justify-center font-bold flex-col items-center">
+                <div className="flex md:flex-row  md:gap-12 justify-center font-bold flex-col gap-4 items-center">
                     {News.map((New) => (
                         <Card2
                             key={New.id}

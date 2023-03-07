8fc9ec359ce362ec303e3c837f92fb828eac3a78
Mansour-VN <pirouz.vn@gmail.com>
2023-03-07 17:30:50 +0330

    fix : add some class for fix responsive in News,New pages

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -10,9 +10,9 @@ const NewsPage = () => {
             <div className="container pt-2">
                 <h1 className="text-4xl font-extrabold text-6xl	text-center leading-[1.5] sticky w-full text-black pb-12">تازه
                     ترین اخبارمالیاتی </h1>
-                <div className="flex md:flex-row md:flex-wrap md:gap-12 justify-center font-bold flex-col gap-4  px-4">
+                <div className="flex flex-col md:flex-row md:flex-wrap md:gap-12 justify-center font-bold flex-col gap-4  px-4">
                     {News.map((New) => (
-                        <div key={New.id} className="w-1/4">
+                        <div key={New.id} className="md:w-1/4">
                             <Card2 report={New}  path={`News/${New.title}`}/>
                         </div>
                     ))}

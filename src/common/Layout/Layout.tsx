430e2c3192af7ad6f9fc90a663907a61f265881d
Morteza <morteza.abolfathi73@gmail.com>
2023-03-14 18:07:05 +0330

    feat : change style Header Desktop

--- src/common/Layout/Layout.tsx
+++ src/common/Layout/Layout.tsx
@@ -1,19 +1,15 @@
 import Footer from "./../components/Footer";
 import Header from "./../components/Header";
-import SocialMedial from "./../components/SocialMedia";
 
 export default function Layout({ children }: {
     children: React.ReactNode,
 }) {
     return (
         <>
-            <div className="header_Layout sticky top-0 z-50 bg-slate-800 ">
+            <div className="header_Layout sticky top-0 z-50 md:mt-10">
                 <Header />
-                <div className="pb-4 md:hidden">
-                  <SocialMedial/>
-                </div>
             </div>
-            <main>{children}</main>
+            <main className="md:relative md:bottom-40">{children}</main>
             <div className="footer_layout">
                 <Footer />
             </div>
--- src/common/components/Header/index.jsx
+++ src/common/components/Header/index.jsx
@@ -1,52 +1,47 @@
-import React, {useState} from 'react'
+import React, { useRef, useState } from "react";
 import Image from "next/image";
-import SocialMedial from '../SocialMedia';
 import DeskTopMenu from "./DeskTopMenu";
 import MobileMenu from "./MobileMenu";
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
 
-    const [menu, setMenu] = useState(false);
-
-    return (
-        <div>
-            <nav
-                className={`w-fulltransition  duration-1000 ease-in-out  md:p-0 bg-slate-800 text-white flex items-center justify-center md:justify-around
-                   md:font-bold ${menu ? 'h-screen  ' : 'md:h-24 h-20 '} `}
-            >
-                <div className={`${menu ? 'hidden' : 'fixed top-8 right-8 bock'}`}>
-                    <Image
-                        src="/assets/images/Logos/DianHamrah.jpg"
-                        alt="لوگو"
-                        width={40}
-                        height={40}
-                        className='rounded animate-bounce '/>
-
-                </div>
-                <div className={`${menu? "hidden": "flex flex-col"}` }>
-                    <h5 className="text-xl">
-                        دیان همراه فردا
-                    </h5>
-                    <span className="badge badge-sm">تنها معتمد مالیاتی نوع دو و سه کشور</span>
-                </div>
-
-                {/*deskTop*/}
-                <div className="hidden md:flex">
-                    <DeskTopMenu setMenu={setMenu} menu={menu}/>
-                </div>
-
-                {/* Mobile responsive*/}
-                <div className="flex md:hidden">
-                    <MobileMenu setMenu={setMenu} menu={menu}/>
-                </div>
-
-                <div className="hidden md:block">
-                    <SocialMedial/>
-                </div>
-            </nav>
+  return (
+    <div className="md:flex md:justify-center">
+      <nav  
+        className={`md:w-3/4  duration-1000 ease-in-out  md:p-0 md:rounded-lg md:bg-opacity-40 bg-slate-800 text-white flex items-center justify-center md:justify-around
+                   md:font-bold ${menu ? "h-screen  " : "md:h-24 h-20 "} `}
+      >
+        <div className={`${menu ? "hidden" : "flex flex-row items-center  "}`}>
+          <div className={`${menu ? "hidden" : ""}`}>
+            <Image
+              src="/assets/images/Logos/DianHamrah.jpg"
+              alt="لوگو"
+              width={40}
+              height={40}
+              className="rounded m-2"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h5 className="text-xl">دیان همراه فردا</h5>
+            <span className="badge badge-sm">
+              تنها معتمد مالیاتی نوع دو و سه کشور
+            </span>
+          </div>
         </div>
-    )
-}
 
+        {/*deskTop*/}
+        <div className="hidden md:flex">
+          <DeskTopMenu setMenu={setMenu} menu={menu} />
+        </div>
+
+        {/* Mobile responsive*/}
+        <div className="flex md:hidden">
+          <MobileMenu setMenu={setMenu} menu={menu} />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;

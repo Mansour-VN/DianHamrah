25b0939365a55a757c2b6b620cb25492678d8e49
morteza <morteza.abolfathi73@gmail.com>
2023-03-15 14:53:43 +0330

    fix: delete animate text on video in homePage

--- src/module/HomePage/Demo/index.tsx
+++ src/module/HomePage/Demo/index.tsx
@@ -11,30 +11,9 @@ const section1 = () => {
                 loop
                 muted
                 autoPlay
-                className="w-full opacity-70"
+                className="w-full"
             />
 
-            <div className="absolute bg-gray-100 opacity-70 rounded-md md:p-4">
-                <TypeAnimation
-                    // Same String at the start will only be typed once, initially
-                    sequence={[
-                        'شرکت دیان‌ همراه‌ فردا',
-                        1000,
-                        'شرکت دیان‌ همراه‌ فردا اولین و تنها‌ترین معتمد نوع دو و سه سازمان‌ امور مالیاتی کشور',
-                        1000,
-                        'شرکت دیان‌ همرا‌ه فردا شما را آموزش می‌دهد که خودتان صورت حساب ارسال کنید',
-                        1000,
-                        'شرکت دیان همراه فردا با پشتیبانی تیم فنی در کنار شما است',
-                        1000,
-                    ]}
-                    speed={50} // Custom Speed from 1-99 - Default Speed: 40
-                    // style={{ fontSize: '2em' }}
-                    wrapper="span" // Animation will be rendered as a <span>
-                    repeat={Infinity} // Repeat this Animation Sequence infinitely
-                    className="text-xl md:text-3xl font-semibold"
-                />
-            </div>
-            
         </div>
     )
 }

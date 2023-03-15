d95e3a50f797703138766110a2d228503116d890
Mansour-VN <pirouz.vn@gmail.com>
2023-03-15 14:48:01 +0330

    Revert "Revert "fix : fix style in homePage""
    This reverts commit b2ee9bc88c9336aa8229049d393b3a6456ea8f2d.

--- src/module/HomePage/index.tsx
+++ src/module/HomePage/index.tsx
@@ -4,7 +4,7 @@ import Requirement from "./Requirement";
 import LawTerminals from "./LawTerminals";
 import CallAndCounseling from "./CallAndCounseling";
 import TaxNews from "./TaxNews";
-import OrganizationIcon from "./OrganizationalIcon";
+import OrganizationIcon from "./CustomersIcons";
 
 const Home = () => {
   return (

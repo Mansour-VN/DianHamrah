d0026ce709027f5b697db6343cd15210788d5511
Mansour-VN <pirouz.vn@gmail.com>
2023-04-09 15:34:53 +0430

    feat : add toast in app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
-import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import Layout from "@/common/Layout/Layout";
 import { NextPage } from "next";
 import { ReactNode, ReactElement } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { QueryClient, QueryClientProvider, } from "react-query";
 
 type NextPageWithLayoutAndAuth = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -24,6 +25,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return getLayout(
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ToastContainer />
     </QueryClientProvider>
   );
 }

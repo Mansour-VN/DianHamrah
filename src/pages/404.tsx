cc3f04c2e7b2b9939120932efc8ccd57a3d52907
Mansour-VN <pirouz.vn@gmail.com>
2023-04-09 15:20:42 +0430

    refactor : remove the preposition in Page404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ const Page404 = () => {
 
         <div id="page-404"
              className="w-full flex md:px-0 px-8 mx-auto flex-col gap-12 justify-center items-center relative bg-[#857387] py-64 z-0 text-center ">
-            <p className="md:text-8xl text-4xl px- font-extrabold">
+            <p className="md:text-8xl text-4xl font-extrabold">
                 {About.title}
             </p>
             <p className="text-8xl container font-extrabold flex  justify-center items-center gap-8">

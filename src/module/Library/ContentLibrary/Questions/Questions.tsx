30b7be182a68ce770445dd4b8bb0daa25ecc8024
Mansour-VN <pirouz.vn@gmail.com>
2023-04-13 21:45:46 +0430

    feat : add related content

--- src/module/Library/ContentLibrary/Questions/Questions.tsx
+++ src/module/Library/ContentLibrary/Questions/Questions.tsx
@@ -1,18 +1,18 @@
-import { Library } from "public/Constants/Library"
+import { FrequentlyQuestions } from "public/Constants/FrequentlyQuestions"
 
 
 export const Questions = ()=>{
     return(
         <div>
-            {Library.map((item)=>{
+            {FrequentlyQuestions.map((item)=>{
 
                 return(
-                    <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                    <div key={item.id} tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mt-6">
                         <div className="collapse-title text-xl font-medium">
-                            Focus me to see content
+                            {item.title}
                         </div>
                         <div className="collapse-content">
-                            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                            <p>{item.description}</p>
                         </div>
                     </div>
                 )
--- src/pages/Library/Content/Questions/index.tsx
+++ src/pages/Library/Content/Questions/index.tsx
@@ -1,9 +1,22 @@
 import {Questions} from "@/module/Library/ContentLibrary/Questions/Questions";
+import Link from "next/link";
 
 const QuestionsPage = () => {
     return (
-        <div className="flex flex-col items-center">
-            <Questions/>
+        <div className="flex flex-col md:flex-row justify-between container mx-auto pt-36 	">
+
+            <div className="w-8/12">
+                <p className="pb-12 font-extrabold text-3xl text-center ">سوالات متداول</p>
+                <Questions/>
+            </div>
+            <div className="border-2 radios-lg w-3/12 h-fit	">
+                <p className="pb-12 font-extrabold text-3xl text-center pt-4">مطالب مفید</p>
+                <div className="border-t-2 flex flex-col p-4">
+                    <Link href='https://dian.s3.ir-thr-at1.arvanstorage.ir/gica.ir.pdf'>امضا الکترونیک</Link>
+                    <Link href='https://dian.s3.ir-thr-at1.arvanstorage.ir/راهنمای-شناسه-کالا-و-خدمات.pdf'>راهنمای-شناسه کالا و خدمات</Link>
+
+                </div>
+            </div>
         </div>
     )
 }
